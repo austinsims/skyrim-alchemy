65f246617fad2859b9e5d2b34ee97618da8505d7
Austin Sims <adsims2001@gmail.com>
2018-03-23 21:03:43 -0500

    increment, decrement, clear

--- lib/presentation/home_screen.dart
+++ lib/presentation/home_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:skyrim_alchemy/actions.dart';
 import 'package:skyrim_alchemy/alchemy/common.dart';
 import 'package:skyrim_alchemy/alchemy/ingredients.dart';
 import 'package:skyrim_alchemy/model.dart';
-import 'package:flutter/foundation.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({Key key}): super(key: key);
@@ -14,9 +15,15 @@ class HomeScreen extends StatelessWidget {
       appBar: new AppBar(
         title: new Text('Ingredients'),
         actions: [
-          new IconButton(icon: new Icon(Icons.clear_all), onPressed: () {
-            // TODO: Clear ingredients.
-          }),
+          new StoreConnector<AppState, VoidCallback>(
+            converter: (store) {
+              return () => store.dispatch(new InitCountsAction());
+            },
+            builder: (context, callback) => new IconButton(
+              icon: new Icon(Icons.clear_all),
+              onPressed: callback,
+            )
+          ),
           new IconButton(icon: new Icon(Icons.local_pharmacy), onPressed: () {
             // TODO: Go to potion list route.
           }),
@@ -44,16 +51,28 @@ class HomeScreen extends StatelessWidget {
     return new ListTile(
       title: new Text(ingredient.name),
       trailing: new Row(children: [
-        new IconButton(icon: new Icon(Icons.remove), onPressed: () {
-          // TODO: Dispatch decrement action
-        }),
+        new StoreConnector<AppState, VoidCallback>(
+          converter: (store) {
+            return () => store.dispatch(new DecIngredAction(ingredient));
+          },
+          builder: (context, callback) => new IconButton(
+            icon: new Icon(Icons.remove),
+            onPressed: callback
+          ),
+        ),
         new StoreConnector<AppState, int>(
           converter: (store) => store.state.ingredCount[ingredient],
           builder: (context, count) => new Text(count.toString()),
         ),
-        new IconButton(icon: new Icon(Icons.add), onPressed: () {
-          // TODO: Dispatch increment action
-        }),
+        new StoreConnector<AppState, VoidCallback>(
+          converter: (store) {
+            return () => store.dispatch(new IncIngredAction(ingredient));
+          },
+          builder: (context, callback) => new IconButton(
+            icon: new Icon(Icons.add),
+            onPressed: callback
+          ),
+        ),
       ]),
     );
   }
--- lib/reducers/ingred_count_reducer.dart
+++ lib/reducers/ingred_count_reducer.dart
@@ -2,24 +2,27 @@ import 'package:redux/redux.dart';
 import 'package:skyrim_alchemy/actions.dart';
 import 'package:skyrim_alchemy/alchemy/common.dart';
 import 'package:skyrim_alchemy/alchemy/ingredients.dart';
+import 'dart:math' show max;
 
 final ingredCountReducer = combineTypedReducers<Map<Ingredient, int>>([
-  new ReducerBinding<Map<Ingredient, int>, IncIngredAction>(_incIngred),
-  new ReducerBinding<Map<Ingredient, int>, DecIngredAction>(_decIngred),
-  new ReducerBinding<Map<Ingredient, int>, InitCountsAction>(_initCounts),
+  new ReducerBinding<Map<Ingredient, int>, IncIngredAction>(_inc),
+  new ReducerBinding<Map<Ingredient, int>, DecIngredAction>(_dec),
+  new ReducerBinding<Map<Ingredient, int>, InitCountsAction>(_init),
 ]);
 
-Map<Ingredient, int> _incIngred(Map<Ingredient, int> state, IncIngredAction action) {
-  // TODO
-  return state;
+Map<Ingredient, int> _inc(Map<Ingredient, int> state, IncIngredAction action) {
+  var copy = new Map<Ingredient, int>.from(state);
+  copy[action.ingredient] = copy[action.ingredient] + 1;
+  return copy;
 }
 
-Map<Ingredient, int> _decIngred(Map<Ingredient, int> state, DecIngredAction action) {
-  // TODO
-  return state;
+Map<Ingredient, int> _dec(Map<Ingredient, int> state, DecIngredAction action) {
+  var copy = new Map<Ingredient, int>.from(state);
+  copy[action.ingredient] = max(0, copy[action.ingredient] - 1);
+  return copy;
 }
 
-Map<Ingredient, int> _initCounts(Map<Ingredient, int> state, InitCountsAction action) {
+Map<Ingredient, int> _init(Map<Ingredient, int> state, InitCountsAction action) {
   var map = new Map();
   for (var ingredient in allIngredients) {
     map[ingredient] = 0;
